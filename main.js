420e87936fd304fe4bec57d03fd3e2cc36b9ed2c
raheemelassadi <raheemelassadi1@gmail.com>
2024-01-02 18:44:12 -0800

    created canvas on window load

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-import './style.css'
-import { setupCounter } from './counter.js'
+
+
+function draw () {
+  const canvas = document.getElementById("#canvas");
+
+  if (canvas.getContext){
+    const ctx = canvas.getContext("2d");
+  }
+}
+
+window.addEventListener("load", draw);
 
 
 
